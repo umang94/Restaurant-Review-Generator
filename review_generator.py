58ee46301df28faf1d4f9e98e6720ec1785cfc8f
Umang Jain <umang94@gmail.com>
2017-11-12 20:34:07 +0530

    Adding graceful handling of invalid restaurant names

--- review_generator.py
+++ review_generator.py
@@ -2,25 +2,35 @@ from urllib.parse import quote
 import requests
 from pprint import pprint
 from summarizer import summarize
+from textblob import TextBlob
 import sys
 
 # ID for Hyderabad, India
+# TODO : Implement global search with restaurant options
 city_id = "6"
 
+# User Inputted Values
 restaurant_name = sys.argv[2]
-
 api_key = sys.argv[1]
+
+#Constants
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": api_key}
-
-
 baseRequestURL= "https://developers.zomato.com/api/v2.1/"
 
 def search(restaurantName,cityId):
     search_param = "search?entity_id=" + cityId + "&entity_type=city&q=" + quote(restaurantName)
     search_request_url = baseRequestURL + search_param
-    response = requests.get(search_request_url,headers=header)
+    try:
+        response = requests.get(search_request_url,headers=header)
+    except Exception as e:
+        raise Exception("Remote Call to Zomato failed")
+
     search_response = (response.json())
+    if(response.status_code != 200):
+        raise Exception("Remote call to zomato failed")
     
+    if len(search_response['restaurants']) == 0:
+        raise Exception("No matching restaurants found in Zomato for restaruant " + restaurantName)
     restaurant_match = search_response['restaurants'][0]['restaurant']['name']
     restaurant_id = search_response['restaurants'][0]['restaurant']['id']
     
