ad80ba00e45dbf2143e9448d86d03a583fc72fbc
Umang Jain <umang94@gmail.com>
2017-11-12 20:51:10 +0530

    Adding support to generate positve/negative reviews

--- README.md
+++ README.md
@@ -7,16 +7,21 @@ A small utility to generate a new review for a restaurant based on the existing 
 Preferred Python version: 3.3
 ## Python dependencies
 1. urllib
-2. requests
-3. summarizer 
+2. requests: http://docs.python-requests.org/en/master/#
+3. summarizer: https://pypi.python.org/pypi/summarizer/0.0.7
+4. textblob: https://textblob.readthedocs.io/en/dev/quickstart.html 
 
-`$ pip3 install urllib,requests,summarizer`
+`$ pip3 install urllib,requests,summarizer,textblob`
 ## Zomato API keys
 
 Generate an API key for yourself at https://developers.zomato.com/api 
 
 # Usage
-python3 review_generator.py "YOUR_API_KEY" "Restaurant Name"
+## Positive review
+`python3 review_generator.py "YOUR_API_KEY" "Restaurant Name" "postive"`
+## Negative review
+`python3 review_generator.py "YOUR_API_KEY" "Restaurant Name" "negative"`
+
 
 
 
--- review_generator.py
+++ review_generator.py
@@ -12,6 +12,7 @@ city_id = "6"
 # User Inputted Values
 restaurant_name = sys.argv[2]
 api_key = sys.argv[1]
+required_sentiment = sys.argv[3]
 
 #Constants
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": api_key}
@@ -20,6 +21,8 @@ baseRequestURL= "https://developers.zomato.com/api/v2.1/"
 def search(restaurantName,cityId):
     search_param = "search?entity_id=" + cityId + "&entity_type=city&q=" + quote(restaurantName)
     search_request_url = baseRequestURL + search_param
+
+    # Bunch of try catches to handle invalid inputs gracefully
     try:
         response = requests.get(search_request_url,headers=header)
     except Exception as e:
@@ -31,6 +34,8 @@ def search(restaurantName,cityId):
     
     if len(search_response['restaurants']) == 0:
         raise Exception("No matching restaurants found in Zomato for restaruant " + restaurantName)
+
+    # Zomato returns the restaurant in order of the closest matches to inputted name, Hence, picking the first one.
     restaurant_match = search_response['restaurants'][0]['restaurant']['name']
     restaurant_id = search_response['restaurants'][0]['restaurant']['id']
     
@@ -48,14 +53,29 @@ def getSummarizedReview(reviews):
     for review in reviews:
         combined_review_text += review["review"]["review_text"]
     new_review_list = summarize("Review",combined_review_text)
-    summarized_review = ("\n".join(new_review_list))
-    print(summarized_review)
+
+    pos_reviews = []
+    neg_reviews = []
+    for summarized_review in new_review_list:
+        textBlob = TextBlob(summarized_review)
+        if(textBlob.sentiment.polarity > 0):
+            pos_reviews.append(summarized_review)
+        else:
+            neg_reviews.append(summarized_review)
+
+
+    output_review = None
+    if(required_sentiment == "negative"):
+        output_review = ("\n".join(neg_reviews))
+    else:
+        # Default behavior is to generate a positive review
+        output_review = ("\n".join(pos_reviews))
+
+    print(output_review)
     return summarized_review
 
-
-
 res_name, res_id = search(restaurant_name,city_id)
-print("Generating a review for " + res_name)
+print("Generating a " + required_sentiment + " review for " + res_name)
 reviews = get_reviews(res_id)
 
 getSummarizedReview(reviews)
